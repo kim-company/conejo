9bd078991a1ad1e277c3f9f0286f703601393634
Philip Giuliani <philip.giuliani@keepinmind.info>
2018-02-08 11:38:04 +0100

    Fix matching of the vhost

--- lib/conejo/connection.ex
+++ lib/conejo/connection.ex
@@ -39,8 +39,8 @@ defmodule Conejo.Connection do
     password = Confex.get_env(:conejo, :password)
 
     vhost = case Confex.fetch_env(:conejo, :vhost) do
-      {:ok, custom_vhost} -> "/#{custom_vhost}"
       {:ok, "/"} -> ""
+      {:ok, custom_vhost} -> "/#{custom_vhost}"
       :error -> ""
       _ -> ""
     end
