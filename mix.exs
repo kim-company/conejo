6e56977366f092c683bb6b6c7cf3b11e95bb7c18
Philip Giuliani <philip.giuliani@keepinmind.info>
2018-02-08 08:57:20 +0100

    Downcase amqp

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule Conejo.Mixfile do
   end
 
   defp deps do
-    [{:amqp, "~> 1.0.0-pre.2"},
+    [{:amqp, "~> 0.2.1"},
     {:confex, "~> 3.2.0"},
     {:earmark, ">= 0.0.0", only: :dev},
     {:ex_doc, ">= 0.0.0", only: :dev}]
